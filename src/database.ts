68aae6d131563b9d1993a98335ec944b70af8353
tupaginaonline <contacto@tupaginaonline.net>
2020-04-11 03:11:09 -0430

    conecc db

--- src/database.ts
+++ src/database.ts
@@ -4,9 +4,10 @@ export const connect = async (): Promise<Pool> => {
 	
 	 const connection = await createPool({
 		
-		host: 'localhost',
-		user: 'root',
-		database: 'nomina2020bd',
+		host: 'bffjve8qdffidcq0larv-mysql.services.clever-cloud.com',
+		user: 'u9i1muywsyxeo4fq',
+		password:'Fa0wjWMJLhe161kgoPeW',
+		database: 'bffjve8qdffidcq0larv',
 		connectionLimit: 10
 		
 	});
